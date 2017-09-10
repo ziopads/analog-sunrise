1eeab1d8a5cea7c791b2ec144e9832fd96b18613
KaneLabs <ryankane28@gmail.com>
2017-09-10 14:28:24 -0600

    TDD with Request/Response

--- src/main/java/com/example/demo/RyansController.java
+++ src/main/java/com/example/demo/RyansController.java
@@ -1,4 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class RyansController {
+
+    @RestController
+    public class RyansController {
+
+        @GetMapping("/ryan")
+        public String hello() {
+        return "Hello World";
+    }
 }
