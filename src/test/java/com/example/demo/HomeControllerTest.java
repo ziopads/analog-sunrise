f156de7a2d65f3b5c7235efc285653b578ebb11b
J. Haskins <ziopads@gmail.com>
2017-09-10 14:33:27 -0600

    Testing for feature 6

--- src/test/java/com/example/demo/HomeControllerTest.java
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -4,19 +4,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoApplicationTests {
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-	@Test
-	public void contextLoads() {
-	}
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeControllerTest.class)
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHomeController() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/");
+
+        this.mvc.perform(request).andExpect(content().string(""));
+    }
+
 
 }
-
