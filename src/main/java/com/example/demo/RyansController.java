c153c1bb47f7fc2a535eeee9a0ec9c31b33dac19
KaneLabs <ryankane28@gmail.com>
2017-09-10 14:30:10 -0600

    syntax error fixed

--- src/main/java/com/example/demo/RyansController.java
+++ src/main/java/com/example/demo/RyansController.java
@@ -3,13 +3,12 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class RyansController {
 
-    @RestController
-    public class RyansController {
-
-        @GetMapping("/ryan")
-        public String hello() {
+    @GetMapping("/ryan")
+    public String hello() {
         return "Hello World";
     }
 }
